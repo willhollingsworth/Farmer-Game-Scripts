f8d5c93ed95efd2d63c3a1097e50d1cf4590ef2c
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-08-06 16:32:26 +0930

    finished maze solving logic

--- Scripts/Maze.py
+++ Scripts/Maze.py
@@ -1,1 +1,26 @@
-../../../../AppData/LocalLow/TheFarmerWasReplaced/TheFarmerWasReplaced/Saves/Save0/Maze.py
+from __builtins__ import *
+from Functions import *
+
+def maze_move(old_orientation = 0):
+    direction_names = [North, East, South, West]
+    for direction_raw in range(-1, 3):
+        direction_corrected = (old_orientation + direction_raw) % 4
+        direction_name = direction_names[direction_corrected]
+        quick_print(direction_raw,direction_corrected)
+        # quick_print('attempting to move', direction_name)
+        if move(direction_name):
+            new_orientation = direction_corrected
+            # quick_print('success', direction_name)
+            return new_orientation
+
+while True:
+    goto_start()
+    spawn_maze()
+
+    orientation = 0
+    for step in range(200):
+        orientation = maze_move(orientation)
+        treasure = get_entity_type() == Entities.Treasure
+        if treasure:
+            harvest()
+            break
