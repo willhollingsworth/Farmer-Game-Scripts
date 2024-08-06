57ee0e5d1c4c53e578d41c0c1392f874be13ce77
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-08-06 16:44:15 +0930

    adding some maze comments

--- Scripts/Maze.py
+++ Scripts/Maze.py
@@ -2,9 +2,11 @@ from __builtins__ import *
 from Functions import *
 
 def maze_move(old_orientation = 0):
+    # given an orientation try all possible movements starting with a right movement
     direction_names = [North, East, South, West]
     for direction_raw in range(-1, 3):
         direction_corrected = (old_orientation + direction_raw) % 4
+        # ensure range wraps correctly over the directions names using a modulo 
         direction_name = direction_names[direction_corrected]
         # quick_print('attempting to move', direction_name)
         if move(direction_name):
@@ -18,6 +20,7 @@ while True:
 
     orientation = 0
     for step in range(200):
+        # navigate a maze by following the right wall
         orientation = maze_move(orientation)
         treasure = get_entity_type() == Entities.Treasure
         if treasure:
